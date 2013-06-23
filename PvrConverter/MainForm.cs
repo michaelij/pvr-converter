c748395c06ba10d589db259d4b9b4f7dfbfe020b
Michael J <michael@shadowfox.net>
2013-06-23 20:53:36 +1000

    Set the application up to work from the command line or with a GUI. The program is a console application that will not load the form if command line arguments are given, or if none are given it will detach from the console and hide the GUI

--- PvrConverter/MainForm.cs
+++ PvrConverter/MainForm.cs
@@ -12,9 +12,31 @@ namespace PvrConverter
 {
     public partial class MainForm : Form
     {
+        /// <summary>
+        /// Whether we want a GUI or the command line only.
+        /// True if command line arguments are provided on startup.
+        /// </summary>
+        private bool closeForCommandLine;
+
         public MainForm()
         {
+            closeForCommandLine = false;
             InitializeComponent();
+        }
+        
+        public MainForm(string[] args)
+            : this()
+        {
+            closeForCommandLine = true;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (closeForCommandLine == true)
+            {
+                this.Close();
+                return;
+            }
         }
     }
 }
--- PvrConverter/Program.cs
+++ PvrConverter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +9,31 @@ namespace PvrConverter
 {
     static class Program
     {
+        [DllImport("kernel32.dll")]
+        static extern bool FreeConsole();
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+
+            // Do we have any command line arguments?
+            if (args.Length != 0)
+            {
+                // Arguments are present. The form will close itself and
+                // leave only the command line available.
+                Application.Run(new MainForm(args));
+            }
+            else
+            {
+                // We want a GUI, so detatch from the console to hide it.
+                FreeConsole();
+                Application.Run(new MainForm());
+            }
         }
     }
 }
